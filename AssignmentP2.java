1f7e9d59496438f95d8286f0eb6bd9306521a2fe
chintalasreepriya12 <chinthalasreepriya123@gmail.com>
2026-08-13 01:16:00 +0000

    Java Assignment1-1.2

--- AssignmentP2.java
+++ AssignmentP2.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class StringAnalyzer{
+class AssignmentP2{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String sentence = sc.nextLine();
